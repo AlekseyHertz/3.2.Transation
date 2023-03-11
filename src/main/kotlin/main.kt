4a22e14a11031e3a4686c256110acac02d827cfb
Aleksey_Hertz <TihAleksey@rambler.ru>
2023-03-11 12:36:29 +0500

    Chanched main

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,17 +1,24 @@
 fun main() {
-    val userTransfer = totalTransfer(100_000_000, "MasterCard")
+    val userTransfer = totalTransfer(transfer = 75_000, userCard = "Maestro")
     println("Сумма комиссии: $userTransfer")
 }
 
-fun totalTransfer(transfer: Int, userCard: String) {
-    var userByCard = transfer
-    when (userCard) {
-        "MasterCard || Maestro" -> if (transfer < 75_000) { userByCard = 0 }
-        else {(transfer * 0.006)+20}
-
-        "Visa || Mir" -> if (transfer * 0.075 < 35) {userByCard = 35}
-        else {transfer * 0.075}
-
-        //else -> 0
+fun totalTransfer(transfer: Int, userCard: String) = when (userCard) {
+    "MasterCard", "Maestro" -> {
+        if (transfer < 75_000) {
+            0
+        } else {
+            ((transfer * 0.006) + 20).toInt()
+        }
     }
+
+    "Visa", "Mir" -> {
+        if (transfer * 0.075 < 35) {
+            35
+        } else {
+            (transfer * 0.075).toInt()
+        }
+    }
+
+    else -> 0
 }
