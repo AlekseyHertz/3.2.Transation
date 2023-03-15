564d51ffd59308b47fec4b5ce1d5b21b7cbd1205
Aleksey_Hertz <TihAleksey@rambler.ru>
2023-03-15 22:46:53 +0500

    "changed code"

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,24 +1,44 @@
 fun main() {
-    val userTransfer = totalTransfer(transfer = 75_000, userCard = "Maestro")
+    val userTransfer = totalTransfer(transfer = 75_000, userCard = "Visa", mounthLimit = 50_000)
     println("Сумма комиссии: $userTransfer")
 }
 
-fun totalTransfer(transfer: Int, userCard: String) = when (userCard) {
+fun totalTransfer(transfer: Int, userCard: String, mounthLimit: Int) = when (userCard) {
     "MasterCard", "Maestro" -> {
-        if (transfer < 75_000) {
-            0
-        } else {
-            ((transfer * 0.006) + 20).toInt()
+        when {
+            mounthLimit < 600_000 -> {
+                if (transfer < 75_000) {
+                    0
+                } else {
+                    ((transfer * 0.006) + 20).toInt()
+                }
+            }
+
+            else -> ("Внимание! Сумма месячных переводов по $userCard превышена")
         }
     }
 
     "Visa", "Mir" -> {
-        if (transfer * 0.075 < 35) {
-            35
-        } else {
-            (transfer * 0.075).toInt()
+        when {
+            mounthLimit < 600_000 -> {
+                if (transfer * 0.075 < 35) {
+                    35
+                } else {
+                    (transfer * 0.075).toInt()
+                }
+            }
+
+            else -> ("Внимание! Сумма месячных переводов по $userCard превышена")
         }
     }
 
-    else -> 0
+    "VK_pay" -> {
+        when {
+            mounthLimit < 40_000 -> 0
+
+            else -> ("Внимание! Сумма месячных переводов по $userCard превышена")
+        }
+    }
+
+    else -> ("Внимание! внесены не корректные данные")
 }
